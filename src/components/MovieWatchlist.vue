ac34360eecd94c0be4a5dc0c5cb2777aa94a8cb1
Grey Milton <grey.milton@gmail.com>
2024-08-04 21:00:48 +0930

    OMDB-14: improve styling of watchlist an search results pages

--- src/components/MovieWatchlist.vue
+++ src/components/MovieWatchlist.vue
@@ -33,56 +33,56 @@ defineEmits(['selection']);
 </script>
 
 <template>
-  <!-- List Container -->
-  <ul
-    v-if="watchlist.length"
-    class="scrollbar overflow-auto sm:h-[906px] md:h-[658px] lg:h-[560px]"
-  >
-    <li
-      v-for="movie in watchlist"
-      :key="movie.imdbID"
-      class="border-t border-zinc-300 first:border-none"
-    >
-      <article>
-        <!-- Movie Selection Button -->
-        <button
-          class="flex w-full items-start justify-between gap-3 p-6 ring-inset hover:bg-zinc-100 focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-neutral-300 active:bg-zinc-200"
-          :class="{ 'bg-zinc-100': selectedMovie === movie.imdbID }"
-          :aria-label="'Select ' + movie.Title"
-          @click="$emit('selection', movie.imdbID)"
-        >
-          <div class="flex items-center gap-3.5">
-            <!-- Poster Image -->
-            <img
-              :src="movie.Poster"
-              alt="Poster for {{ movie.Title }}"
-              class="h-16 w-16 rounded-md object-cover object-center"
-            />
-            <div class="pb-1.5">
-              <!-- Title -->
-              <h1 class="text-left text-lg font-extralight">
-                {{ movie.Title }}
-              </h1>
-              <!-- Year -->
-              <p class="text-left text-sm font-extralight text-zinc-400">
-                {{ movie.Year }}
-              </p>
+  <div class="scrollbar overflow-auto lg:h-[560px]">
+    <!-- List -->
+    <ul v-if="watchlist.length">
+      <li
+        v-for="movie in watchlist"
+        :key="movie.imdbID"
+        class="border-t border-zinc-300 first:border-none"
+      >
+        <article>
+          <!-- Movie Selection Button -->
+          <button
+            class="flex w-full items-start justify-between gap-3 p-6 ring-inset hover:bg-zinc-100 focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-neutral-300 active:bg-zinc-200"
+            :class="{ 'bg-zinc-100': selectedMovie === movie.imdbID }"
+            :aria-label="'Select ' + movie.Title"
+            @click="$emit('selection', movie.imdbID)"
+          >
+            <div class="flex items-center gap-3.5">
+              <!-- Poster Image -->
+              <img
+                :src="movie.Poster"
+                alt="Poster for {{ movie.Title }}"
+                class="h-16 w-16 rounded-md object-cover object-center"
+              />
+              <div class="pb-1.5">
+                <!-- Title -->
+                <h1 class="text-left text-lg font-extralight">
+                  {{ movie.Title }}
+                </h1>
+                <!-- Year -->
+                <p class="text-left text-sm font-extralight text-zinc-400">
+                  {{ movie.Year }}
+                </p>
+              </div>
             </div>
-          </div>
-          <!-- Bookmark Icon -->
-          <BookmarkIcon
-            class="mt-1 h-5 w-5 flex-shrink-0 self-start fill-yellow-500 stroke-yellow-500 stroke-2"
-          />
-        </button>
-      </article>
-    </li>
-  </ul>
-  <p
-    v-else
-    class="mb-6 p-12 text-center font-extralight"
-  >
-    Your watchlist is empty.
-  </p>
+            <!-- Bookmark Icon -->
+            <BookmarkIcon
+              class="mt-1 h-5 w-5 flex-shrink-0 self-start fill-yellow-500 stroke-yellow-500 stroke-2"
+            />
+          </button>
+        </article>
+      </li>
+    </ul>
+    <!-- List empty message -->
+    <p
+      v-else
+      class="mb-6 p-12 text-center font-extralight"
+    >
+      Your watchlist is empty.
+    </p>
+  </div>
 </template>
 
 <style>
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -38,48 +38,50 @@ defineEmits(['selection']);
 </script>
 
 <template>
-  <!-- List Container -->
-  <ul class="scrollbar overflow-auto sm:h-[906px] md:h-[658px] lg:h-[560px]">
-    <li
-      v-for="result in results"
-      :key="result.imdbID"
-      class="border-t border-zinc-300 first:border-none"
-    >
-      <article>
-        <!-- Result Item Button -->
-        <button
-          class="flex w-full items-start justify-between gap-3 p-6 ring-inset hover:bg-zinc-100 focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-neutral-300 active:bg-zinc-200"
-          :class="{ 'bg-zinc-100': selectedResult === result.imdbID }"
-          :aria-label="'Select ' + result.Title"
-          @click="$emit('selection', result.imdbID)"
-        >
-          <div class="flex items-center gap-3.5">
-            <!-- Poster Image -->
-            <img
-              :src="result.Poster"
-              alt="Poster for {{ result.Title }}"
-              class="h-16 w-16 rounded-md object-cover object-center"
-            />
-            <div class="pb-1.5">
-              <!-- Title -->
-              <h1 class="text-left text-lg font-extralight">
-                {{ result.Title }}
-              </h1>
-              <!-- Year -->
-              <p class="text-left text-sm font-extralight text-zinc-400">
-                {{ result.Year }}
-              </p>
+  <div class="scrollbar overflow-auto lg:h-[560px]">
+    <!-- List -->
+    <ul v-if="results.length">
+      <li
+        v-for="result in results"
+        :key="result.imdbID"
+        class="border-t border-zinc-300 first:border-none"
+      >
+        <article>
+          <!-- Result Item Button -->
+          <button
+            class="flex w-full items-start justify-between gap-3 p-6 ring-inset hover:bg-zinc-100 focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-neutral-300 active:bg-zinc-200"
+            :class="{ 'bg-zinc-100': selectedResult === result.imdbID }"
+            :aria-label="'Select ' + result.Title"
+            @click="$emit('selection', result.imdbID)"
+          >
+            <div class="flex items-center gap-3.5">
+              <!-- Poster Image -->
+              <img
+                :src="result.Poster"
+                alt="Poster for {{ result.Title }}"
+                class="h-16 w-16 rounded-md object-cover object-center"
+              />
+              <div class="pb-1.5">
+                <!-- Title -->
+                <h1 class="text-left text-lg font-extralight">
+                  {{ result.Title }}
+                </h1>
+                <!-- Year -->
+                <p class="text-left text-sm font-extralight text-zinc-400">
+                  {{ result.Year }}
+                </p>
+              </div>
             </div>
-          </div>
-          <!-- Bookmark Icon -->
-          <BookmarkIcon
-            v-if="watchlist.includes(result.imdbID)"
-            class="mt-1 h-5 w-5 flex-shrink-0 self-start fill-yellow-500 stroke-yellow-500 stroke-2"
-          />
-        </button>
-      </article>
-    </li>
-  </ul>
+            <!-- Bookmark Icon -->
+            <BookmarkIcon
+              v-if="watchlist.includes(result.imdbID)"
+              class="mt-1 h-5 w-5 flex-shrink-0 self-start fill-yellow-500 stroke-yellow-500 stroke-2"
+            />
+          </button>
+        </article>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style>
