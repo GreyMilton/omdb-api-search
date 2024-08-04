33ef67c0bba553dc2dd6e65a31ad69b878641e49
Grey Milton <grey.milton@gmail.com>
2024-08-04 14:46:46 +0930

    OMDB-13: create & use CrossIcon.vue as close button in MovieDetails.vue

--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CrossIcon from './icons/CrossIcon.vue';
 import BookmarkIcon from './icons/BookmarkIcon.vue';
 
 defineProps({
@@ -17,48 +18,50 @@ defineEmits(['close', 'toggle-on-watchlist']);
 
 <template>
   <article
-    class="border-l border-zinc-300 px-8 pb-8 pt-1 text-lg font-extralight lg:p-8"
+    class="relative border-l border-zinc-300 px-8 pb-8 pt-1 text-lg font-extralight lg:p-8"
   >
+    <button
+      class="absolute right-1 top-1 hidden rounded-full p-2 text-sm hover:bg-zinc-100 focus-visible:bg-zinc-50 focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-neutral-300 active:bg-zinc-200 lg:block"
+      aria-label="Close"
+      @click="$emit('close')"
+    >
+      <CrossIcon
+        aria-hidden="true"
+        class="h-3 w-3 stroke-zinc-500 stroke-[1.6]"
+      />
+    </button>
     <button
       class="my-3 block pb-2 pl-1 pr-2 pt-2 text-sm hover:bg-zinc-100 focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-neutral-300 active:bg-zinc-200 lg:hidden"
       @click="$emit('close')"
     >
       &lt; back to results
     </button>
-    <div class="flex flex-col gap-4 pb-6 sm:flex-row">
+    <div class="flex flex-col gap-10 pb-6 sm:flex-row sm:gap-4">
       <img
         :src="movie.Poster"
-        class="h-80 w-52 flex-none overflow-hidden rounded-md object-cover"
+        class="flex-none overflow-hidden rounded-md object-cover sm:max-h-80 sm:max-w-52 sm:self-center"
       />
-      <div class="flex grow flex-col justify-center gap-8 lg:justify-start">
-        <div class="flex flex-col items-end self-end">
-          <button
-            class="-mt-2 hidden p-2 text-sm hover:bg-zinc-100 focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-neutral-300 active:bg-zinc-200 lg:block"
-            @click="$emit('close')"
-          >
-            close
-          </button>
-          <button
-            aria-label="Toggle on/off watchlist"
-            class="flex items-center justify-center gap-2 rounded-[4px] border px-3 py-2.5 font-normal focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-neutral-300"
+      <div class="flex grow flex-col justify-center gap-8 md:justify-start">
+        <button
+          aria-label="Toggle on/off watchlist"
+          class="flex items-center justify-center gap-2 self-end rounded-[4px] border px-3 py-2.5 font-normal focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-neutral-300"
+          :class="
+            watchlist.includes(movie.imdbID)
+              ? 'border-yellow-500 bg-amber-50 text-yellow-600 hover:bg-[#fff7d7] active:bg-yellow-50'
+              : 'border-black hover:bg-zinc-100 active:bg-zinc-50'
+          "
+          @click="$emit('toggle-on-watchlist', movie.imdbID)"
+        >
+          <BookmarkIcon
+            class="h-6 w-6 stroke-2"
             :class="
               watchlist.includes(movie.imdbID)
-                ? 'border-yellow-500 bg-amber-50 text-yellow-600 hover:bg-[#fff7d7] active:bg-yellow-50'
-                : 'border-black hover:bg-zinc-100 active:bg-zinc-50'
+                ? 'fill-yellow-500 stroke-yellow-500'
+                : 'fill-none stroke-black'
             "
-            @click="$emit('toggle-on-watchlist', movie.imdbID)"
-          >
-            <BookmarkIcon
-              class="h-6 w-6 stroke-2"
-              :class="
-                watchlist.includes(movie.imdbID)
-                  ? 'fill-yellow-500 stroke-yellow-500'
-                  : 'fill-white stroke-black'
-              "
-            />
-            Watchlist
-          </button>
-        </div>
+          />
+          Watchlist
+        </button>
         <div class="space-y-3">
           <h1 class="text-4xl font-semibold">{{ movie.Title }}</h1>
           <p class="text-neutral-600">
---
+++ src/components/icons/CrossIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+    viewBox="0 0 10 10"
+  >
+    <path d="m1 1 8 8m-8 0 8-8" />
+  </svg>
+</template>
