618118fc4cd241bb711bcfd1d122a80ba2b07aa9
Grey Milton <grey.milton@gmail.com>
2024-08-07 05:39:25 +0930

    OMDB-16: rearrange tests in SearchYearFIlter.test.js

--- tests/SearchYearFilter.test.js
+++ tests/SearchYearFilter.test.js
@@ -3,50 +3,56 @@ import { describe, it, expect } from 'vitest';
 import SearchYearFilter from '../src/components/SearchYearFilter.vue';
 
 describe('SearchYearFilter.vue', () => {
-  const wrapper = mount(SearchYearFilter, {
-    props: {
-      year: '1999',
-      enabled: true,
-    },
-  });
+  describe('enabled', () => {
+    const wrapper = mount(SearchYearFilter, {
+      props: {
+        year: '1999',
+        enabled: true,
+      },
+    });
+    const currentYear = new Date().getFullYear();
+    const checkboxLabel = wrapper.find('label[for="enable-year"]');
+    const checkbox = wrapper.find('input[type="checkbox"]');
+    const yearLabel = wrapper.find('label[for="year-input"]');
+    const yearInput = wrapper.find('input[type="range"]');
+    const yearDisplay = wrapper.find('p');
 
-  const currentYear = new Date().getFullYear();
-  const checkboxLabel = wrapper.find('label[for="enable-year"]');
-  const checkbox = wrapper.find('input[type="checkbox"]');
-  const yearLabel = wrapper.find('label[for="year-input"]');
-  const yearInput = wrapper.find('input[type="range"]');
-  const yearDisplay = wrapper.find('p');
+    it('renders html elements correctly', () => {
+      expect(checkboxLabel.exists()).toBe(true);
+      expect(checkboxLabel.text()).toBe('enable');
 
-  it('renders html elements correctly', () => {
-    expect(checkboxLabel.exists()).toBe(true);
-    expect(checkboxLabel.text()).toBe('enable');
+      expect(checkbox.exists()).toBe(true);
+      expect(checkbox.element.checked).toBe(true);
 
-    expect(checkbox.exists()).toBe(true);
-    expect(checkbox.element.checked).toBe(true);
+      expect(yearLabel.exists()).toBe(true);
+      expect(yearLabel.text()).toBe('Year');
 
-    expect(yearLabel.exists()).toBe(true);
-    expect(yearLabel.text()).toBe('Year');
+      expect(yearInput.exists()).toBe(true);
+      expect(yearInput.attributes('min')).toBe('1900');
+      expect(yearInput.attributes('max')).toBe(currentYear.toString());
+      expect(yearInput.element.value).toBe('1999');
 
-    expect(yearInput.exists()).toBe(true);
-    expect(yearInput.attributes('min')).toBe('1900');
-    expect(yearInput.attributes('max')).toBe(currentYear.toString());
-    expect(yearInput.element.value).toBe('1999');
+      expect(yearDisplay.text()).toBe('1999');
+    });
 
-    expect(yearDisplay.text()).toBe('1999');
-  });
+    it('correctly changes the year model value on selection', () => {
+      expect(wrapper.vm.year).toBe('1999');
 
-  it('correctly changes the year model value on selection', () => {
-    expect(wrapper.vm.year).toBe('1999');
-
-    wrapper
-      .find('input[type="range"]')
-      .setValue('2000')
-      .then(() => {
+      yearInput.setValue('2000').then(() => {
         expect(wrapper.vm.year).toBe('2000');
       });
+    });
   });
 
   it('correctly toggles the enabled model value when checkbox value changes', () => {
+    const wrapper = mount(SearchYearFilter, {
+      props: {
+        year: '1999',
+        enabled: true,
+      },
+    });
+    const checkbox = wrapper.find('input[type="checkbox"]');
+
     expect(wrapper.vm.enabled).toBe(true);
     expect(checkbox.element.checked).toBe(true);
 
