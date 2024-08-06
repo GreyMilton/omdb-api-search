cdb381ea7b4c3b33fe31717a9407c09230a3eb0c
Grey Milton <grey.milton@gmail.com>
2024-08-07 05:41:54 +0930

    OMDB-16: write & pass tests for when enabled is false in SearchYearFilter.test.js

--- tests/SearchYearFilter.test.js
+++ tests/SearchYearFilter.test.js
@@ -44,6 +44,47 @@ describe('SearchYearFilter.vue', () => {
     });
   });
 
+  describe('disabled', () => {
+    const wrapper = mount(SearchYearFilter, {
+      props: {
+        year: '1999',
+        enabled: false,
+      },
+    });
+    const currentYear = new Date().getFullYear();
+    const checkboxLabel = wrapper.find('label[for="enable-year"]');
+    const checkbox = wrapper.find('input[type="checkbox"]');
+    const yearLabel = wrapper.find('label[for="year-input"]');
+    const yearInput = wrapper.find('input[type="range"]');
+    const yearDisplay = wrapper.find('p');
+
+    it('renders html elements correctly', () => {
+      expect(checkboxLabel.exists()).toBe(true);
+      expect(checkboxLabel.text()).toBe('enable');
+
+      expect(checkbox.exists()).toBe(true);
+      expect(checkbox.element.checked).toBe(false);
+
+      expect(yearLabel.exists()).toBe(true);
+      expect(yearLabel.text()).toBe('Year');
+
+      expect(yearInput.exists()).toBe(true);
+      expect(yearInput.attributes('min')).toBe('1900');
+      expect(yearInput.attributes('max')).toBe(currentYear.toString());
+      expect(yearInput.element.value).toBe('1999');
+
+      expect(yearDisplay.text()).toBe('1999');
+    });
+
+    it('cannot change the year model value on selection', () => {
+      expect(wrapper.vm.year).toBe('1999');
+
+      yearInput.setValue('2000').then(() => {
+        expect(wrapper.vm.year).toBe('1999');
+      });
+    });
+  });
+
   it('correctly toggles the enabled model value when checkbox value changes', () => {
     const wrapper = mount(SearchYearFilter, {
       props: {
