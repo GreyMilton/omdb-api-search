0898dcd64335ee0d6a91c479a2f9b85db270a8c2
Grey Milton <grey.milton@gmail.com>
2024-08-07 21:19:23 +0930

    OMDB-16: write & pass App.test.js 'computes watchlistIds correctly'

--- tests/App.test.js
+++ tests/App.test.js
@@ -46,4 +46,43 @@ describe('App.vue', () => {
       expect(wrapper.findComponent(MovieDetails).exists()).toBe(true);
     });
   });
+
+  it('computes watchlistIds correctly', () => {
+    const wrapper = createWrapper();
+
+    const movies = [
+      {
+        Title: 'Star Wars: Episode IV - A New Hope',
+        Year: '1977',
+        imdbID: 'tt0076759',
+        Type: 'movie',
+        Poster:
+          'https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg',
+      },
+      {
+        Title: 'Star Wars: Episode V - The Empire Strikes Back',
+        Year: '1980',
+        imdbID: 'tt0080684',
+        Type: 'movie',
+        Poster:
+          'https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+      },
+      {
+        Title: 'Star Wars: Episode VI - Return of the Jedi',
+        Year: '1983',
+        imdbID: 'tt0086190',
+        Type: 'movie',
+        Poster:
+          'https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+      },
+    ];
+
+    wrapper.vm.watchlist = movies;
+
+    expect(wrapper.vm.watchlistIds).toEqual([
+      movies[0].imdbID,
+      movies[1].imdbID,
+      movies[2].imdbID,
+    ]);
+  });
 });
