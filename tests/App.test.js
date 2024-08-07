847ef9ce7d7533e9b137668f518679e8e882675a
Grey Milton <grey.milton@gmail.com>
2024-08-07 21:53:25 +0930

    OMDB-16: write & pass App.test.js 'correctly hides load more button when there are no more results to featch'

--- tests/App.test.js
+++ tests/App.test.js
@@ -127,4 +127,21 @@ describe('App.vue', () => {
       expect(loadMoreButton).toBe(undefined);
     });
   });
+
+  it('correctly hides load more button when there are no more results to featch', () => {
+    const wrapper = createWrapper();
+
+    new Promise((resolve) => {
+      wrapper.vm.totalResults = 10;
+      wrapper.vm.showWatchlist = false;
+      wrapper.vm.searchResults = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      resolve();
+    }).then(() => {
+      const loadMoreButton = wrapper
+        .findAll('button[type="button"]')
+        .filter((button) => button.text() === 'Load more...')[0];
+
+      expect(loadMoreButton).toBe(undefined);
+    });
+  });
 });
